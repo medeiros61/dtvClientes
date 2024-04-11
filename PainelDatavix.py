1391ef89a3bfebacd6c46e00fe9c192a3338d6ba
lucasendlichgomes <lucasendlich@gmail.com>
2024-04-11 16:45:23 -0300

    logica atualização

--- PainelDatavix.py
+++ PainelDatavix.py
@@ -1,13 +1,23 @@
 import customtkinter as ctk
 from tkinter import *
-
+import cliente as cl
 def DataVix():
     screen_datavix = ctk.CTk()
     screen_datavix.geometry("1100x580")
     screen_datavix.title("Master")
     screen_datavix.resizable(False,False)
-    username_entry = ctk.CTkEntry(master=screen_datavix, placeholder_text="TESTE", width=300, font=("Roboto",14))
-    username_entry.place(x=500, y=200)
+    #username_entry = ctk.CTkEntry(master=screen_datavix, placeholder_text="TESTE", width=300, font=("Roboto",14))
+    #username_entry.pack(side=RIGHT)
+    frame_clientes = ctk.CTkFrame(master=screen_datavix, width=900, height=580, fg_color=("#809090"))
+
+    def ativarmei():
+        cl.Removertelaclientes(frame_clientes)
+    
+    def ativarclietes():
+        
+        cl.criartelaclientes(frame_clientes)
+            
+
     #aba
     options_frame = ctk.CTkFrame(master=screen_datavix, width=200, height=580, fg_color=("#808080"))
     options_frame.pack(side=LEFT)
@@ -16,10 +26,10 @@ def DataVix():
     botao_dashboard = ctk.CTkButton(master=options_frame, text="DASHBOARD")
     botao_dashboard.place(x=32, y=250)
 
-    botao_clientes = ctk.CTkButton(master=options_frame, text="CLIENTES")
+    botao_clientes = ctk.CTkButton(master=options_frame, text="CLIENTES",command=ativarclietes)
     botao_clientes.place(x=32, y=300)
 
-    botao_MEI = ctk.CTkButton(master=options_frame, text="CONTROLE MEis")
+    botao_MEI = ctk.CTkButton(master=options_frame, text="CONTROLE MEis", command=ativarmei)
     botao_MEI.place(x=32, y=350)
 
     botao_usuarios = ctk.CTkButton(master=options_frame, text="USUARIOS")
@@ -31,3 +41,4 @@ def DataVix():
 #---------------------------------------------------------------------------------#
 
 
+DataVix()
---
+++ cliente.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+from tkinter import *
+
+def criartelaclientes(frame):
+    frame.pack(side=RIGHT)
+
+
+def Removertelaclientes(frame): 
+    frame.pack_forget()
